7309fad743e14bc91319265ff4edf7b46d50238c
liushaoquan <liushaoquan@aiyunxiao.com>
2020-12-23 11:58:56 +0800

    dev: preview image & show loading

--- app.js
+++ app.js
@@ -3,17 +3,20 @@ const express = require('express')
 const app = express();
 app.set('view engine', 'pug')
 app.set('views', './views')
+var serveIndex = require('serve-index')
 app.get('/', function (req, res) {
   res.render('index', { title: 'Hey', message: 'Hello there!' })
+  
 })
 let path = require('path')
 let public = path.join(__dirname, 'public')
+let test = path.join(__dirname, 'test')
 app.use(express.static(public))
+app.use('/images', express.static(test), serveIndex(test, {'icons': true}))
 let clientList = [];
-function tellAll(clientList, data) {
- 
+function tellAll(clientList, eventName, data) {
   clientList.forEach(client => {
-    client.write(`event: notice\n`);
+  client.write(`event: ${eventName}\n`);
     client.write(`data: ${JSON.stringify({msg: JSON.stringify(data)})}\n\n`);
   })
 }
@@ -22,18 +25,18 @@ app.post('/test', (req, res) => {
   
   ls.stdout.on('data', (data) => {
     console.log(`stdout: ${data}`);
-    tellAll(clientList, data)
+    tellAll(clientList, 'notice', data)
   });
 
   ls.stderr.on('data', (data) => {
     console.log(`stderr: ${data}`);
-    tellAll(clientList, data)
+    tellAll(clientList, 'notice', data)
   });
 
   ls.on('close', (code) => {
     console.log(`child process exited with code ${code}`);
     res.end('thank you')
-    tellAll(clientList, 'thank you')
+    tellAll(clientList, 'end', 'thank you')
   })
 })
 
--- public/index.js
+++ public/index.js
@@ -10,14 +10,28 @@ function updateTest() {
 }
 
 var term = new Terminal({
-  cols: 200
+  cols: 200,
 });
+
 term.open(document.getElementById("terminal"));
 term.write("Hello from \x1B[1;3;31mxterm.js\x1B[0m $ ");
 
 const event = new EventSource("/connect");
+let isLoading = false;
+const btnUpdateTest = document.getElementById("btn-update-test");
 
+function updateButtonState() {
+  if (isLoading) {
+    btnUpdateTest.classList.add("is-loading");
+  } else {
+    btnUpdateTest.classList.remove("is-loading");
+  }
+}
 event.addEventListener("notice", (e) => {
+  if (!isLoading) {
+    isLoading = true;
+    updateButtonState();
+  }
   // let data = JSON.parse(e.data);
   let data = e.data;
   data = JSON.parse(data);
@@ -26,13 +40,17 @@ event.addEventListener("notice", (e) => {
   if (typeof msg === "object") {
     let data = new Uint8Array(msg.data);
     data = new TextDecoder("utf-8").decode(data);
-    data = data.split('\n')
-    data.forEach(line => {
+    data = data.split("\n");
+    data.forEach((line) => {
       console.log(line);
       term.write(line + "\n\r");
-    })
+    });
   } else {
     console.log(data);
     term.write(data + "\n\r");
   }
+});
+event.addEventListener("end", (e) => {
+  isLoading = false;
+  updateButtonState();
 });
