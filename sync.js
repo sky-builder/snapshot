d4f0a1390df2d944f815388821e15fa929b3deea
liushaoquan <liushaoquan@aiyunxiao.com>
2020-12-25 17:45:02 +0800

    fix: sync.js

--- sync.js
+++ sync.js
@@ -1,31 +1,38 @@
 /**
  * 将 test 文件夹里的测试用例转成 JSON 格式, 用于前端展示和选择测试范围
  */
-const fs = require('fs')
-const path = require('path')
+const fs = require("fs");
+const path = require("path");
+const ignoreFiles = ['config.js', 'index.js'];
 
-let result = [];
-
-let root = path.join(__dirname, 'test')
-
-let users = fs.readdirSync(root)
-users = users.filter(u => u !== 'config.js' && u !== 'index.js')
-for(let i = 0; i < users.length; i += 1) {
-  let p =  path.join(root, users[i]);
-  let exams = fs.readdirSync(p);
-  exams = exams.filter(u => u !== 'config.js' && u !== 'index.js')
-  let k = {};
-  k.userId = users[i];
-  k.examList = [];
-  for(let j = 0; j < exams.length; j += 1) {
-    let r = {};
-    r.examId = exams[j];
-    let f = path.join(root, users[i], exams[j], 'report-list.json');
-    let config = require(f)
-    r.reportList = config;
-    k.examList.push(r)
-  }
-  result.push(k);
+function filterFiles(files) {
+  let result = files.filter((u) => !ignoreFiles.includes(u));
+  return result;
 }
-fs.writeFileSync('sync.json', JSON.stringify(result))
 
+function main() {
+  let result = [];
+  let rootDir = path.join(__dirname, "test");
+  let users = fs.readdirSync(rootDir);
+  users = filterFiles(users);
+  for (let i = 0; i < users.length; i += 1) {
+    let userDir = path.join(rootDir, users[i]);
+    let exams = fs.readdirSync(userDir);
+    exams = filterFiles(exams);
+    let u = {};
+    u.userId = users[i];
+    u.examList = [];
+    for (let j = 0; j < exams.length; j += 1) {
+      let e = {};
+      e.examId = exams[j];
+      let examReportListPath = path.join(rootDir, users[i], exams[j], "report-list.json");
+      let examReportList = require(examReportListPath);
+      e.reportList = examReportList;
+      u.examList.push(e);
+    }
+    result.push(u);
+  }
+  fs.writeFileSync("sync.json", JSON.stringify(result));
+}
+
+main();
