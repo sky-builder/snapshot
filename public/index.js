64363f6e08c5363006e01aa4de0c4aa6882c1ddc
liushaoquan <liushaoquan@aiyunxiao.com>
2020-12-28 14:09:45 +0800

    dev

--- public/index.js
+++ public/index.js
@@ -23,7 +23,6 @@ var term = new Terminal({
 });
 
 term.open(document.getElementById("terminal"));
-term.write("Hello from \x1B[1;3;31mxterm.js\x1B[0m $ ");
 
 const event = new EventSource("/connect");
 let isLoading = false;
